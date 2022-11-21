54414c13600618c0b697ec31c82afe9cbfa38222
Quadrago <thequadrago@gmail.com>
2022-11-21 18:24:34 -0500

    added doc strings

--- StoredQuestions.java
+++ StoredQuestions.java
@@ -3,23 +3,33 @@ import java.util.ArrayList;
 
 public class StoredQuestions extends StoredData {
 
-    private int numQuestions = 0;
+    private int numQuestions;
 
+    /**
+     * call super constructor and set current question count
+     * @param file containing questions
+     */
     public StoredQuestions(String file) {
-
-        this.file = file;
-        storeData();
+        super(file); 
+        numQuestions = 0;
     }
+    
     @Override
     protected void storeLine(String line) {
+
+        //remove any lines containg Question headers
         if(line.toUpperCase().contains("Q"+(numQuestions+1))) {
-            data.add(new ArrayList<String>());
+            data.add(new ArrayList<String>()); //new empty arraylist added (new line in array)
             numQuestions++;
             return;
         }
+
+        //remove blank lines
         else if(line.equals("")) {
             return;
         }
+
+        //removes spaces and sets line to lowercase
         data.get(numQuestions-1).add(line.toLowerCase().replace(" ", ""));
     }
     
