d64eaf035d0abefa695729705f7f27eb010c5ff3
Dav <gameskokswa@gmail.com>
2022-11-22 01:57:04 -0500

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      
+
+        Facade work = new Facade();
+        work.setAnswerData();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("To skip to answers type 'yes' and 'no' to solve questions");
+        String accessAnswers = scan.nextLine();
+        if(accessAnswers.equals("no")) { 
+            //to solve questions
+        //question file entry
+            System.out.println("Enter file name containing question data (including file extension): ");
+            work.setQuestionData();
+        }
+        else if (accessAnswers.equals("yes")) {
+            work.compareToAnswers();
+        }
+        scan.close();
+        work.outScores();
+        work.outAnswers();
     }
 }
